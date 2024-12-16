bb2d2516c64a7d3e9be6dc112268ebfa92facf46
Anyerrr <anyer.moreno@estudiantat.upc.edu>
2024-12-16 19:18:35 +0100

    Task #215: Username guardat a context

--- src/context/ApiKeyContext.js
+++ src/context/ApiKeyContext.js
@@ -1,14 +1,16 @@
 import React, { createContext, useState } from 'react';
 
-// Crea el contexto
+// Crear el contexto
 export const ApiKeyContext = createContext();
 
 // Proveedor del contexto
 export const ApiKeyProvider = ({ children }) => {
+    // Inicializar estado para API key y username
     const [apiKey, setApiKey] = useState('');
+    const [username, setUsername] = useState('');
 
     return (
-        <ApiKeyContext.Provider value={{ apiKey, setApiKey }}>
+        <ApiKeyContext.Provider value={{ apiKey, setApiKey, username, setUsername }}>
             {children}
         </ApiKeyContext.Provider>
     );
