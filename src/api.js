cd426321930582939fc140537e3abcfdc1adf07d
Anyerrr <anyer.moreno@estudiantat.upc.edu>
2024-12-19 11:40:41 +0100

    Apunta a local

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://puma-hackers-8a28327c331a.herokuapp.com/', // URL de tu API
+  baseURL: 'http://127.0.0.1:8000/', // URL de tu API
 });
 
 export default api;
